d5aea29029a21a34b19ea0e02d5b46b4685878ef
Dan Radez <dradez@redhat.com>
2019-02-21 12:42:50 -0500

    Adding create_vlan and delete_vlan

--- network_runner/api.py
+++ network_runner/api.py
@@ -20,7 +20,14 @@ import ansible_runner
 
 from network_runner import exceptions
 from network_runner.resources.ansible import Playbook
+from network_runner.resources.ansible.playbook import Play
+from network_runner.resources.ansible.playbook import Task
 from network_runner.resources.inventory import Inventory
+
+IMPORT_ROLE = 'import_role'
+NETWORK_RUNNER = 'network-runner'
+NETWORK_CLI = 'network_cli'
+
 
 class NetworkRunner(object):
     """Object to invoke ansible_runner to call Ansible Networking
@@ -46,3 +53,52 @@ class NetworkRunner(object):
             raise exceptions.AnsibleRunnerException(' '.join(result.stdout))
 
         return result
+
+    def create_vlan(self, hostname, vlan_id, vlan_name=None):
+        """Create VLAN.
+
+        :param hostname: The name of the host in Ansible inventory.
+        :param vlan_id: The VLAN's ID to create.
+        :param vlan_name: The VLAN's name/description.
+        """
+        play = Play(name='Create VLAN using {}'.format(NETWORK_CLI),
+                    hosts=hostname,
+                    connection=NETWORK_CLI,
+                    gather_facts=False)
+
+        task = Task(name='Create VLAN',
+                    module=IMPORT_ROLE,
+                    args={'name': NETWORK_RUNNER,
+                          'tasks_from': 'create_vlan'},
+                    vars={'vlan_id': vlan_id})
+
+        play.tasks.add(task)
+
+        playbook = Playbook()
+        playbook.add(play)
+
+        return self.run(playbook)
+
+    def delete_vlan(self, hostname, vlan_id):
+        """Delete VLAN.
+
+        :param hostname: The name of the host in Ansible inventory.
+        :param vlan_id: The VLAN's ID to delete.
+        """
+        play = Play(name='Delete VLAN using {}'.format(NETWORK_CLI),
+                    hosts=hostname,
+                    connection=NETWORK_CLI,
+                    gather_facts=False)
+
+        task = Task(name='Delete VLAN',
+                    module=IMPORT_ROLE,
+                    args={'name': NETWORK_RUNNER,
+                          'tasks_from': 'delete_vlan'},
+                    vars={'vlan_id': vlan_id})
+
+        play.tasks.add(task)
+
+        playbook = Playbook()
+        playbook.add(play)
+
+        return self.run(playbook)

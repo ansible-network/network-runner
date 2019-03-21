6d8faf94b111f8979c936fe34a9283f3e43669cb
Dan Radez <dradez@redhat.com>
2019-03-20 22:42:11 -0400

    updating directory stucture and version to build 0.1.1 as an rpm

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def main():
 
     setuptools.setup(
         name='network-runner',
-        version='0.1.0',
+        version='0.1.1',
         description='Abstracton and Python API for Ansible Networking',
         author='Ansible',
         author_email='info@ansible.com',

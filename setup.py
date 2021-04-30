f3ca541f512a88c2bf05b9a9e167f58007f6b339
Dan Radez <dradez@redhat.com>
2021-04-30 12:27:34 -0400

    messed up tagging, bumping to 0.3.4 to match a new tag

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def main():
 
     setuptools.setup(
         name='network-runner',
-        version='0.3.0',
+        version='0.3.4',
         description='Abstracton and Python API for Ansible Networking',
         long_description=long_description,
         author='Ansible',
